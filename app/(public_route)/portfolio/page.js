a9293af40bbc23261753cb9b75472b4b31d6577b
Alisadaintanvir <alisadaintanvir@gmail.com>
2024-08-03 17:12:47 +0600

    add portfolio page

--- app/(public_route)/portfolio/page.js
+++ app/(public_route)/portfolio/page.js
@@ -1,5 +1,26 @@
-function page() {
-  return <div>Portfolio</div>;
+import Image from "next/image";
+function PortfolioPage() {
+  return (
+    <div className="flex items-start justify-center">
+      <div className="card bg-base-100  shadow-xl">
+        <figure className=" pt-10">
+          <Image
+            src="/images/01.jpg"
+            alt="Shoes"
+            className="rounded-xl"
+            width={400}
+            height={200}
+          />
+        </figure>
+        <div className="card-body items-center text-center">
+          <h2 className="card-title">Shoes!</h2>
+          <div className="card-actions">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default page;
+export default PortfolioPage;
